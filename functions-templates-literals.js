4b397d9606fc8b983acd242925ba826d00906805
Blanket25 <bianca_inga@hotmail.com>
2021-10-25 14:48:58 +0200

    initial commit

---
+++ functions-templates-literals.js
@@ -0,0 +1,25 @@
+//Nivel 1
+function printUserName(name) {
+  console.log("printing solution nivel 1: " + name);
+}
+
+printUserName("Ada");
+
+//Nivel 2
+//Ejercicio 1
+const userName = "Ada";
+const userSurname = "Lovelace";
+
+console.log(
+  `printing solution nivel 2, ejercicio 1: ${userName} ${userSurname}`
+);
+
+//Ejercicio 2
+function returnAge(age) {
+  return `printing solution nivel 2, ejercicio 2: I'm ${age} years old`;
+}
+
+console.log(returnAge(30));
+
+//Nivel 3
+//Ejercicio 1

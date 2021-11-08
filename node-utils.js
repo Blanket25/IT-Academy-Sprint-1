aaa3f9508d70ee09c95525940a8d8a96a46c3677
Blanket25 <bianca_inga@hotmail.com>
2021-11-08 12:01:07 +0100

    node utils nivel 1 and 2 finished

--- node-utils.js
+++ node-utils.js
@@ -1,11 +1,9 @@
 console.log("Node utils:");
-const express = require("express");
-const app = express();
-const fs = require("fs");
+const fs = require("fs-extra");
 const compressing = require("compressing");
+const fileger = require("fileger");
 
 //Nivel 1
-console.log("Nivel 1");
 //Ejercicio 1
 console.log("Nivel 1, ejercicio 1:");
 
@@ -17,31 +15,35 @@ const printMessageEverySec = () => {
 printMessageEverySec();
 
 //Ejercicio 2
-const writeMessageInAnotherFile = () => {
-  fs.writeFile("someText.txt", "Hello, I come from another World!", (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log("Hello, I come from another World! > someText.txt");
-  });
+const message = "Hello, I come from another World!";
+
+const writeMessageInAnotherFile = async () => {
+  try {
+    await fs.writeFile("someText.txt", message, "utf-8");
+    console.log("Nivel 1, ejercicio 2:");
+    console.log("File created");
+  } catch (err) {
+    console.error(err);
+  }
 };
 writeMessageInAnotherFile();
 
 //Ejercicio 3
-console.log("Nivel 1, ejercicio 3:");
-
-const showMessageFromAnotherFile = () => {
-  fs.readFile("someText.txt", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    }
+const showMessageFromAnotherFile = async () => {
+  try {
+    const data = await fs.readFile("someText.txt", "utf-8");
+    console.log("Nivel 1, ejercicio 3:");
     console.log(data);
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 showMessageFromAnotherFile();
 
 //Nivel 2
+console.log("Nivel 2:");
+
 const compFile = () => {
   new compressing.gzip.FileStream({ source: "someText.txt" })
     .on("error", (err) => console.log(err))
@@ -50,3 +52,33 @@ const compFile = () => {
 };
 
 compFile();
+
+const showFilesFromDir = () => {
+  fs.readdir("/users", (err, files) => {
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
+};
+
+showFilesFromDir();
+
+//Nivel 3
+const encodeMessages = async (file) => {
+  try {
+    const hexFile = await fs.readFile(file, "hex");
+    const base64File = await fs.readFile(file, "base64");
+
+    console.log("Nivel 3:");
+
+    await fs.writeFile("someTextHex.txt", hexFile, () =>
+      console.log("File created and encoded(hex)")
+    );
+    await fs.writeFile("someTextBase64.txt", base64File);
+    console.log("File created and encoded(base64)");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+encodeMessages("someText.txt");
